5a70d95efcde4fd0bd7c1320cfbd24905e90d946
Parthiv Reddy Y <parthivreddy7769@gmail.com>
2025-03-30 18:22:15 +0530

    small corrections related to tailwind css

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('./tailwind.config.js'),
+    require('tailwindcss'),
     require('autoprefixer'),
+    require('postcss-preset-env')({
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+      },
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+    }),
   ],
 }
